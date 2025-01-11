05736d64d6f9a5f2308c9452609adcf5a623f701
senzoo-oh <osj3418@gmail.com>
2025-01-11 22:33:39 +0900

    init project

---
+++ src/main/java/senzoo/goalCatcher/GoalCatcherApplication.java
@@ -0,0 +1,13 @@
+package senzoo.goalCatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalCatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalCatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/senzoo/goalCatcher/GoalCatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package senzoo.goalCatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalCatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
